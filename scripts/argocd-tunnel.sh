db5bddfacfbfa29a1285e5c5c249d27614038015
Gena Tokarev <gennadii.tokariev@gmail.com>
2026-08-19 23:10:36 +0200

    script change

--- scripts/argocd-tunnel.sh
+++ scripts/argocd-tunnel.sh
@@ -8,6 +8,8 @@ user=${ARGOCD_SSH_USER:-deploy}
 echo "Argo CD will be available at http://localhost:8080 while this command is running."
 exec ssh \
   -o ExitOnForwardFailure=yes \
+  -o ServerAliveInterval=15 \
+  -o ServerAliveCountMax=3 \
   -L 8080:127.0.0.1:8080 \
   -p "$port" \
   "$user@$host" \
